01dcaa34fd84c14643c60db1292ab0081bb5529b
Matthew Marineau <matoululu@users.noreply.github.com>
2024-06-06 10:21:25 -0400

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import Popup from './source/pop-up';
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOM loaded');
+  const loadedSpeed = document.getElementById('loaded-speed');
+  const [entry] = performance.getEntriesByType("navigation");
+  loadedSpeed.innerHTML = `${entry.domContentLoadedEventStart}ms`;
 });
